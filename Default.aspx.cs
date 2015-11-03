6654488e08e59c4033787a38ff15e710dce15ab8
netdata-admin <admin@netdata.com>
2015-11-03 14:33:16 +0200

    NetMap

---
+++ Default.aspx.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Drawing;
+using System.Net;
+using System.Xml;
+using System.IO;
+using System.Data;
+
+public partial class _Default : System.Web.UI.Page
+{
+    public static string AccPo;
+    public static string Xml;
+    public static DataSet ds;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    [WebMethod]
+    public static void SaveProject(string accpo, string xml)
+    {
+        AccPo = accpo;
+        Xml = xml;
+    }
+    [WebMethod]
+    public static string GetData()
+    {
+        ds = new DataSet();
+        ds.ReadXml(Xml);
+        char[] labels = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'V', 'W', 'Y', 'Z' };
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        foreach (DataRow row in ds.Tables[0].Rows)
+        {
+            sb.Append("<a onclick=\"ShowPoint('" + i + "')\" class='list-group-item' style='cursor:pointer;'>");
+            sb.Append("<span class='badge'>" + labels[i % labels.Length] + "</span>");
+            sb.Append("<h4 class='list-group-item-heading'>" + row["dc_Name"] + "</h4>");
+            sb.Append("<p class='list-group-item-text'>Latitude:" + row["dc_Latitude"] + "<br/>Longitude:" + row["dc_Longitude"] + "</p>");
+            sb.Append("</a>");
+            i++;
+        }
+        return sb.ToString();
+    }
+    [WebMethod]
+    public static string[] GetMarker()
+    {
+        ds = new DataSet();
+        ds.ReadXml(Xml);
+        string[] data = new string[ds.Tables[0].Rows.Count];
+        int i = 0;
+        foreach (DataRow row in ds.Tables[0].Rows)
+        {
+            data[i] = row["dc_Latitude"].ToString() + "," + row["dc_Longitude"].ToString() + "," + row["dc_Name"] + "," + row["ID"];
+            i++;
+        }
+        return data;
+    }
+    [WebMethod]
+    public static string EditData(string dataid, string name)
+    {
+        try
+        {
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create("http://www.netdata.com/AccPo.asmx");
+            webRequest.Headers.Add(@"SOAP:Action:http://tempuri.org/UpdateRecord");
+            webRequest.ContentType = "text/xml;charset=\"utf-8\"";
+            webRequest.Accept = "text/xml";
+            webRequest.Method = "POST";
+            XmlDocument soapEnvelopeXml = new XmlDocument();
+            string envelope = "<soap:Envelope xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:xsd='http://www.w3.org/2001/XMLSchema' xmlns:soap='http://schemas.xmlsoap.org/soap/envelope/'><soap:Body><UpdateRecord xmlns='http://tempuri.org/'><APIKey>" + AccPo + "</APIKey><IDNumber>" + Convert.ToInt32(dataid) + "</IDNumber><UpdateFieldName>dc_Name</UpdateFieldName><UpdateFieldNewValue>" + name + "</UpdateFieldNewValue></UpdateRecord></soap:Body></soap:Envelope>";
+            soapEnvelopeXml.LoadXml(envelope);
+
+            using (Stream stream = webRequest.GetRequestStream())
+            {
+                soapEnvelopeXml.Save(stream);
+            }
+            WebResponse response = webRequest.GetResponse();
+            return "success";
+        }
+        catch (Exception)
+        {
+            return "exception";
+        }
+    }
+    [WebMethod]
+    public static string DeleteData(string id)
+    {
+        try
+        {
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create("http://www.netdata.com/AccPo.asmx");
+            webRequest.Headers.Add(@"SOAP:Action:http://tempuri.org/DeleteRecord");
+            webRequest.ContentType = "text/xml;charset=\"utf-8\"";
+            webRequest.Accept = "text/xml";
+            webRequest.Method = "POST";
+            XmlDocument soapEnvelopeXml = new XmlDocument();
+            string envelope = "<soap:Envelope xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:xsd='http://www.w3.org/2001/XMLSchema' xmlns:soap='http://schemas.xmlsoap.org/soap/envelope/'><soap:Body><DeleteRecord xmlns='http://tempuri.org/'><APIKey>" + AccPo + "</APIKey><IDNumber>" + id + "</IDNumber></DeleteRecord></soap:Body></soap:Envelope>";
+            soapEnvelopeXml.LoadXml(envelope);
+
+            using (Stream stream = webRequest.GetRequestStream())
+            {
+                soapEnvelopeXml.Save(stream);
+            }
+            WebResponse response = webRequest.GetResponse();
+            return "success";
+        }
+        catch (Exception)
+        {
+            return "exception";
+        }
+    }
+    [WebMethod]
+    public static string SaveData(string name, string lat, string lng)
+    {
+        try
+        {
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create("http://www.netdata.com/AccPo.asmx");
+            webRequest.Headers.Add(@"SOAP:Action:http://tempuri.org/InsertRecord");
+            webRequest.ContentType = "text/xml;charset=\"utf-8\"";
+            webRequest.Accept = "text/xml";
+            webRequest.Method = "POST";
+            XmlDocument soapEnvelopeXml = new XmlDocument();
+            string envelope = "<soap:Envelope xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:xsd='http://www.w3.org/2001/XMLSchema' xmlns:soap='http://schemas.xmlsoap.org/soap/envelope/'><soap:Body><InsertRecord xmlns='http://tempuri.org/'><APIKey>" + AccPo + "</APIKey><InsertList><AccPoKeyValuePair><Key>dc_Latitude</Key><Value>" + lat + "</Value></AccPoKeyValuePair><AccPoKeyValuePair><Key>dc_Longitude</Key><Value>" + lng + "</Value></AccPoKeyValuePair><AccPoKeyValuePair><Key>dc_Name</Key><Value>" + name + "</Value></AccPoKeyValuePair></InsertList></InsertRecord></soap:Body></soap:Envelope>";
+            soapEnvelopeXml.LoadXml(envelope);
+
+            using (Stream stream = webRequest.GetRequestStream())
+            {
+                soapEnvelopeXml.Save(stream);
+            }
+            WebResponse response = webRequest.GetResponse();
+            return "true";
+        }
+        catch (Exception)
+        {
+            return "false";
+        }
+    }
+}
---
+++ js/Netmap.js
@@ -0,0 +1,295 @@
+﻿/// <reference path="jquery.cookie.js" />
+var map; var infowindow;
+var marker = new google.maps.Marker();
+var ltlng = [];
+var markers = [];
+var array = [];
+var newmarkers = [];
+var accpo; var xml;
+$(document).ready(function InitializeMap() {
+    var latlng = new google.maps.LatLng(40.756, -73.986);
+    var myOptions =
+    {
+
+        center: latlng,
+        scrollWheel: false,
+        zoom: 13
+
+    };
+    map = new google.maps.Map(document.getElementById("map"), myOptions);
+    if ($.cookie('xml') != undefined && $.cookie('accpo') != undefined) {
+        accpo = $.cookie('accpo');
+        xml = $.cookie('xml');
+        AddMarker();
+    }
+});
+function accpochange() {
+    $.cookie('accpo', $("#accpo").val().trim());
+    accpo = $.cookie('accpo');
+}
+function xmlchange() {
+    $.cookie('xml', $("#xmllink").val().trim());
+    xml = $.cookie('xml');
+}
+function insertmarker() {
+    markers = [];
+    var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var labelIndex = 0;
+    for (var i = 0; i < ltlng.length; i++) {
+        marker = new google.maps.Marker({
+            map: map,
+            position: ltlng[i],
+            draggable: false,
+            title: array[i].split(',')[2].trim(),
+            content: "<div class='text-center'><h4>" + array[i].split(',')[2].trim() + "</h4></div><hr/><div class='btn-group text-center' style='margin:0 auto'><a class='btn btn-warning btn-sm' data-toggle='modal' data-target='#yeniwizard' onclick='EditPoint(\"" + i + "\")'>Düzenle</a><a class='btn btn-danger btn-sm' onclick='DeletePoint(\"" + array[i].split(',')[3].trim() + "\")'>Sil</a></div>",
+            id: i,
+            dataid: array[i].split(',')[3].trim()
+        });
+        google.maps.event.addListener(marker, 'click', function () {
+            // Calling the open method of the infoWindow 
+            if (!infowindow) {
+                infowindow = new google.maps.InfoWindow();
+            }
+            infowindow.setContent(this.content);
+            infowindow.open(map, this);
+        });
+        markers.push(marker);
+    }
+}
+function AddMarker() {
+    $.ajax({
+        type: "POST",
+        url: "Default.aspx/SaveProject",
+        contentType: "application/json; charset=utf-8",
+        data: "{accpo:'" + accpo + "',xml:'" + xml + "'}",
+        dataType: "json",
+        async: false,
+        cache: false,
+        success: function (response) {
+            DataList();
+            setmarker();
+        },
+        error: function (request, status, error) {
+
+        }
+    });
+    function DataList() {
+        $.ajax({
+            type: "POST",
+            url: "Default.aspx/GetData",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            async: false,
+            cache: false,
+            success: function (response) {
+                $("#mapjs").html(response.d);
+            },
+            error: function (request, status, error) {
+
+            }
+        });
+    }
+    function setmarker() {
+        ltlng = [];
+        $.ajax({
+            type: "POST",
+            url: "Default.aspx/GetMarker",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            async: false,
+            cache: false,
+            success: function (response) {
+                array = response.d;
+                for (var i = 0; i < array.length; i++) {
+                    ltlng.push(new google.maps.LatLng(array[i].split(',')[0].trim(), array[i].split(',')[1].trim()));
+                }
+                insertmarker();
+                map.setCenter(ltlng[ltlng.length - 1]);
+                map.setZoom(8);
+            },
+            error: function (request, status, error) {
+
+            }
+        });
+    }
+    function deletelastmarkers() {
+        for (var i = 0; i < newmarkers.length; i++) {
+            newmarkers[i].setMap(null);
+        }
+    }
+    google.maps.event.addListener(map, 'click', function (event) {
+        //call function to create marker
+
+        //delete the old marker
+        deletelastmarkers();
+        if (markers.length == 0) {
+            setmarker();
+        }
+        marker = new google.maps.Marker({ position: event.latLng, map: map });
+        map.setCenter(marker.position);
+        map.setZoom(12);
+        newmarkers.push(marker);
+        google.maps.event.addListener(marker, 'click', function () {
+
+            if (!infowindow) {
+                infowindow = new google.maps.InfoWindow();
+            }
+            infowindow.setContent(" <h5>Burayı Kaydetmek Istermisiniz ?</h5><div class='text-center'><a data-toggle='modal' data-target='#yeniwizard' onclick='SaveArea(\"" + event.latLng + "\")' class='btn btn-info btn-sm' style='margin:0 auto'>Kaydet</a></div>");
+            infowindow.open(map, marker);
+
+        });
+        //creer à la nouvelle emplacement
+
+    });
+}
+function ShowPoint(index) {
+    map.setCenter(markers[index].position);
+    if (!infowindow) {
+        infowindow = new google.maps.InfoWindow();
+    }
+    infowindow.setContent(markers[index].content);
+    infowindow.open(map, markers[index]);
+    markers[index].setAnimation(google.maps.Animation.j);
+    map.setZoom(12);
+}
+function SaveArea(latlng) {
+    var array = latlng.split(',');
+    var lat = array[0].replace('(', '');
+    var lng = array[1].replace(')', '');
+    $("#lat").val(lat);
+    $("#lng").val(lng);
+    $("#btnedit").hide();
+    $("#btnmodal").show();
+}
+function Save() {
+    var name = $("#txtareaname").val();
+    var lat = $("#lat").val();
+    var lng = $("#lng").val();
+    $.ajax({
+        type: "POST",
+        url: "Default.aspx/SaveData",
+        contentType: "application/json; charset=utf-8",
+        data: "{name:'" + name + "',lat:'" + lat + "',lng:'" + lng + "'}",
+        dataType: "json",
+        async: false,
+        cache: false,
+        success: function (response) {
+            $("#yeniwizard").modal('toggle');
+            location.reload();
+            $("#txtareaname").val("");
+        },
+        error: function (request, status, error) {
+
+        }
+    });
+}
+function EditPoint(index) {
+    $("#txtareaname").val(markers[index].title);
+    $("#lat").val(markers[index].position.lat());
+    $("#lng").val(markers[index].position.lng());
+    $("#dataid").text(markers[index].dataid);
+    $("#btnedit").show();
+    $("#btnmodal").hide();
+}
+function EditData() {
+    var id = $("#dataid").text();
+    var name = $("#txtareaname").val();
+    $.ajax({
+        type: "POST",
+        url: "Default.aspx/EditData",
+        contentType: "application/json; charset=utf-8",
+        data: "{dataid:'" + id + "',name:'" + name + "'}",
+        dataType: "json",
+        async: false,
+        cache: false,
+        success: function (response) {
+            location.reload();
+        },
+        error: function (request, status, error) {
+            console.log(error);
+        }
+    });
+}
+function DeletePoint(id) {
+    swal({
+        title: "Nokta silinsin mi?",
+        text: name,
+        type: "warning",
+        showCancelButton: true,
+        cancelButtonText: "Hayır",
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "Sil",
+        closeOnConfirm: true
+    },
+       function () {
+           $.ajax({
+               type: "POST",
+               url: "Default.aspx/DeleteData",
+               contentType: "application/json; charset=utf-8",
+               data: "{id:" + JSON.stringify(id) + "}",
+               dataType: "json",
+               async: false,
+               cache: false,
+               success: function (response) {
+                   if (response.d) {
+                       location.reload();
+                       swal("Silindi!", "Nokta Başarıyla Silindi.", "success");
+                   }
+                   else {
+                       swal("Hata", "Silme İşlemi Sırasında Bir Hata Meydana Geldi.", "error");
+                   }
+               },
+               error: function (request, status, error) {
+                   swal("Hata", "Bir Hatayla Karşılaşıldı", "error");
+               }
+           })
+       });
+}
+function Ara(address) {
+    geocoder = new google.maps.Geocoder();
+    insertmarker();
+    var address = document.getElementById("aranankelime").value;
+    
+        geocoder.geocode({ 'address': address }, function (results, status) {
+            console.log(results);
+            if (status == google.maps.GeocoderStatus.OK) {
+                map.setCenter(results[0].geometry.location);
+                var marker = new google.maps.Marker({
+                    map: map,
+                    position: results[0].geometry.location
+                });
+                if (results[0].formatted_address) {
+                    region = results[0].formatted_address + '<br />';
+                }
+                var infowindow = new google.maps.InfoWindow({
+                    content: '<div style =width:400px; height:400px;>Konum Bilgisi:<br/>Ülke Adı:<br/>' + region + '<br/>Enlem-Boylam Bilgisi:<br/>' + results[0].geometry.location + '</div>'
+                });
+                google.maps.event.addListener(marker, 'click', function () {
+                    // Calling the open method of the infoWindow 
+                    infowindow.open(map, marker);
+                });
+                google.maps.event.addListener(map, 'click', function (event) {
+                    //delete the old marker
+                    if (marker) {
+                        marker.setMap(null);
+                        insertmarker();
+                    }
+                    //creer à la nouvelle emplacement
+                    marker = new google.maps.Marker({ position: event.latLng, map: map });
+                    google.maps.event.addListener(marker, 'click', function () {
+
+                        if (!infowindow) {
+                            infowindow = new google.maps.InfoWindow();
+                        }
+
+                        infowindow.setContent("<h5>Burayı Kaydetmek Istermisiniz ?</h5> <hr/><div class='text-center'> <a data-toggle='modal' data-target='#yeniwizard' onclick='SaveArea(\"" + event.latLng + "\")' class='btn btn-info btn-sm' style='margin:0 auto'>Kaydet</a></div>");
+                        infowindow.open(map, marker);
+
+                    });
+                });
+            }
+            else {
+                alert("Geocode was not successful for the following reason: " + status);
+            }
+        });
+}
